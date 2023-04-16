49de259788ec4cd283371b92d03625fd7a003af4
Ehsan KhademOlama <72014925+ekoshv@users.noreply.github.com>
2023-04-16 13:37:56 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="colab_api_server",
-    version="0.1.0",
+    version="0.1.1",
     author='Ehsan KhademOlama',
     author_email='ekoshv.igt@gmail.com',
     description='A Python library for setting up a remote Google Colab API server',

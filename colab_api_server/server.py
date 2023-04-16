66c02275e86c00e3e4c1ba2dbc6b21ae918f3487
Ehsan KhademOlama <72014925+ekoshv@users.noreply.github.com>
2023-04-16 14:02:25 +0200

    Update server.py

--- colab_api_server/server.py
+++ colab_api_server/server.py
@@ -1,64 +1,40 @@
 import base64
-import pickle
+import io
 from flask import Flask, request, jsonify, url_for
 from flask_ngrok import run_with_ngrok
 
 class ColabAPIServer:
     def __init__(self):
-        """Initialize the ColabAPIServer class and define routes."""
         self.app = Flask(__name__)
         run_with_ngrok(self.app)
 
         @self.app.route('/execute', methods=['POST'])
         def execute():
-            """
-            Handle the code execution request.
-            :return: The result of the code execution.
-            """
             data = request.get_json(force=True)
-
             code = data.get('code', '')
             input_data = data.get('input_data', {})
-
             result = self.execute_code(code, input_data)
             return jsonify(result)
 
-    def serialize_data(self, data):
-        """Serialize data using pickle and encode it with base64."""
-        serialized_data = pickle.dumps(data)
-        return base64.b64encode(serialized_data).decode('utf-8')
-
-    def deserialize_data(self, data_str):
-        """Decode base64 data and deserialize it using pickle."""
-        decoded_data = base64.b64decode(data_str.encode('utf-8'))
-        return pickle.loads(decoded_data)
-
     def execute_code(self, code, input_data):
-        """
-        Execute the provided code with the given input data.
-        :param code: The code to be executed.
-        :param input_data: The input data for the code execution.
-        :return: The result of the code execution or an error message.
-        """
         try:
             local_namespace = {'input_data': input_data}
-
-            if 'complex_input' in input_data:
-                complex_input = self.deserialize_data(input_data['complex_input'])
-                local_namespace['complex_input'] = complex_input
-
             exec(code, globals(), local_namespace)
+            result_object = local_namespace.get('result', None)
 
-            if 'result' in local_namespace and isinstance(local_namespace['result'], (tuple, list, dict, set)):
-                local_namespace['complex_result'] = self.serialize_data(local_namespace['result'])
-                del local_namespace['result']
+            if result_object is not None:
+                model_buffer = io.BytesIO()
+                result_object.save(model_buffer)
+                model_buffer.seek(0)
+                result_encoded = base64.b64encode(model_buffer.read()).decode('utf-8')
+            else:
+                result_encoded = None
 
-            return local_namespace
+            return {'result': result_encoded}
         except Exception as e:
             return {'error': str(e)}
 
     def run(self):
-        """Run the Colab API server and display the public URL."""
         with self.app.test_request_context():
             public_url = url_for('execute', _external=True)
 
